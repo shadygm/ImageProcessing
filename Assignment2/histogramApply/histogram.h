717853987397159287d46fa192d65dc81c2007e9
Shady <shady.gmira@gmail.com>
2023-10-16 14:01:24 +0200

    finished histogram

--- Assignment2/histogramApply/histogram.h
+++ Assignment2/histogramApply/histogram.h
@@ -6,12 +6,12 @@
 #include <iostream>
 
 #include "../images/myImage.h"
-#define HISTOGRAM_PATH "image/output/histogram.png"
+#define HISTOGRAM_PATH "image/output/histogram.jpg"
 
 using namespace Image;
 namespace histo {
-  void createHistogram(myImage image, uint32_t *histogram);
-  void applyTransformation(myImage image, uint32_t *histogram);
+  void createHistogram(myImage image, uint32_t *histogram, size_t startingVal);
+  void applyTransformation(myImage image, uint32_t *histogram, uint8_t *histoModifiedImage, size_t startingVal);
   void histogramTransformation(myImage image);
 }
 
--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -5,11 +5,15 @@
 const std::string doorPath = "image/input/door.jpg";
 const std::string skiingPath = "image/input/skiing.jpg";
 const std::string smogPath = "image/input/smog.jpg";
+const std::string grayScale = "image/output/grayScale.jpg";
+
 using namespace Image;
+
 int main () {
   myImage image = myImage(smogPath);
 
   image.convertToGrayScale();
+  image.outputImage(grayScale);
   Command::execute("histogram", image);
   image.freeImage();   
 }
